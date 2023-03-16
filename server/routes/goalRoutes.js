c40034820fd482b2168ffc629774b3dfcf27d48e
Danyal Imran <dan.imran97@gmail.com>
2023-03-16 17:54:27 -0400

    added a update and delete feature for users. Users are not able to update or delete each others goals due to auth and JWT

--- server/routes/goalRoutes.js
+++ server/routes/goalRoutes.js
@@ -2,7 +2,9 @@ const express = require('express')
 const router = express.Router()
 const {getGoals, createGoal, updateGoal, deleteGoal} = require('../controllers/goalController')
 
-router.route('/').get(getGoals).post(createGoal)
-router.route('/:id').delete(deleteGoal).put(updateGoal)
+const {protecc} = require('../middleware/authMiddleware')
+
+router.route('/').get(protecc, getGoals).post(protecc, createGoal)
+router.route('/:id').delete(protecc, deleteGoal).put(protecc, updateGoal)
 
 module.exports = router 
