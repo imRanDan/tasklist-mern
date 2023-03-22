31a5c38efabf15503eea7e6eff214572c5c13d72
Danyal Imran <dan.imran97@gmail.com>
2023-03-22 00:12:31 -0400

    commented something out

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ app.use('/api/users', require('./routes/userRoutes'))
 if(process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/build')))
 
-    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'client', 'build', 'index.html')))
+    // app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'client', 'build', 'index.html')))
 } else {
     app.get('/', (req, res) => res.send('Set to production Please'))
 }
