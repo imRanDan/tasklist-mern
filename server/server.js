7d6918a53f1a7ec6fc20d3f498a78d82614c21bc
Danyal Imran <dan.imran97@gmail.com>
2023-03-21 23:31:14 -0400

    added some scripts to serve clientside

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const colors = require('colors')
 const dotenv = require('dotenv').config()
@@ -14,6 +15,15 @@ app.use(express.urlencoded({ extended: false }))
  
 app.use('/api/tasks', require('./routes/taskRoutes'))
 app.use('/api/users', require('./routes/userRoutes'))
+
+// Serve the client
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')))
+
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'client', 'build', 'index.html')))
+} else {
+    app.get('/', (req, res) => res.send('Set to production Please'))
+}
 
 app.use(errorHandler)
 
