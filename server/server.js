25588d87dc72882216d07f45df60c212c3538548
Danyal Imran <dan.imran97@gmail.com>
2023-03-22 10:53:53 -0400

    made a couple more changes to db.js and server.js for cyclic hosting

--- server/server.js
+++ server/server.js
@@ -4,12 +4,15 @@ const colors = require('colors')
 const dotenv = require('dotenv').config()
 const {errorHandler} = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
+
+
+const app = express()
 const port = process.env.PORT || 5003
 
 
 connectDB()
 
-const app = express()
+
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
@@ -17,14 +20,14 @@ app.use(express.urlencoded({ extended: false }))
 app.use('/api/tasks', require('./routes/taskRoutes'))
 app.use('/api/users', require('./routes/userRoutes'))
 
-// Serve the client
-if(process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')))
+// // Serve the client
+// if(process.env.NODE_ENV === 'production') {
+//     app.use(express.static(path.join(__dirname, '../client/build')))
 
-    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'client', 'build', 'index.html')))
-} else {
-    app.get('/', (req, res) => res.send('Set to production Please'))
-}
+//     app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'client', 'build', 'index.html')))
+// } else {
+//     app.get('/', (req, res) => res.send('Set to production Please'))
+// }
 
 app.use(errorHandler)
 
