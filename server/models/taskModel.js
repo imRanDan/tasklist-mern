dff0f5497b585bb55ff3c7467824ef6a77e3ddec
Danyal Imran <dan.imran97@gmail.com>
2023-04-07 14:32:53 -0400

    implemented an assignTo function and trying to get it to work

--- server/models/taskModel.js
+++ server/models/taskModel.js
@@ -1,18 +1,27 @@
 const mongoose = require('mongoose')
 
-const taskSchema = mongoose.Schema(
+const taskSchema = new mongoose.Schema(
 {
   user: {
     type: mongoose.Schema.Types.ObjectId,
     required: true,
     ref: 'User'
   },
-  text: {
+  description: {
     type: String,
     required: [true, 'Please add a text value']
+  },
+  status: {
+    type: String,
+    enum: ['Pending', 'In Progress', 'Completed'],
+    default: 'Pending'
+  },
+  assignedTo: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
   }
 }, {
   timestamps: true,
-})
+});
 
 module.exports = mongoose.model('Task', taskSchema)
