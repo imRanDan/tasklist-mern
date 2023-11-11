8c7d099cc3b402c7a93bf6b4a393b79471de27ef
Danyal Imran <dan.imran97@gmail.com>
2023-11-10 19:33:27 -0500

    working on hero section

--- mytasklist-2.0/src/app/components/Hero.tsx
+++ mytasklist-2.0/src/app/components/Hero.tsx
@@ -4,10 +4,10 @@ const Hero = () => {
   return (
   <section className="bg-gray-100 text-gray-800">
     <div className="container mx-auto flex flex-col items-center px-4 py-16 text-center md:py-32 md:px-10 lg:px-32 xl:max-w-3xl">
-      <h1 className="text-4xl font-bold leading-none sm:text-5xl">Get organized today!
-        <span className="text-blue-600"> myTaskList</span> is for the doers
+      <h1 className="text-4xl font-bold leading-none sm:text-5xl">Elevate your 
+        <span className="text-blue-600"> Productivity</span> with myTaskList
       </h1>
-      <p className="px-8 mt-8 mb-12 text-lg"></p>
+      <p className="px-8 mt-8 mb-12 text-lg">Simplify your life with myTaskList: A user-friendly app that helps you organize your tasks and focus on what matters.</p>
       <div className="flex flex-wrap justify-center">
         <button className="px-8 py-3 m-2 text-lg font-semibold rounded bg-blue-600 text-gray-50">Get started</button>
         <button className="px-8 py-3 m-2 text-lg border rounded text-gray-900 border-gray-300">Learn more</button>
